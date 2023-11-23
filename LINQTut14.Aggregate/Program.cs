fe062ab2ccaa0539402516cc886c93d6dc505d1c
ramsalo1981 <rami.salloum@hotmail.com>
2023-11-23 19:51:16 +0100

    first commit

---
+++ LINQTut14.Aggregate/Program.cs
@@ -0,0 +1,89 @@
+﻿
+using Shared;
+
+namespace LINQTut14.Aggregate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RunMethod01();
+            Console.WriteLine("\n***************");
+            RunMethod02();
+            Console.WriteLine("\n***************");
+            RunMethod03();
+            Console.ReadKey();
+        }
+
+        
+
+        private static void RunMethod01()
+        {
+            var names = new[] { "Ali", "Salem", "Abeer", "Reem", "Jalal" };
+            // Ali, Salem, ....
+
+            //var output = "";
+            //foreach (var item in names)
+            //    output += $"{item},";
+            //Console.WriteLine(output.TrimEnd(','));
+
+            //var output = string.Join(',', names);
+            //Console.WriteLine(output);
+
+
+
+            // a = value befor adding , b = adding value
+            var output = names.Aggregate((a, b) =>
+            {
+                Console.WriteLine($"a = {a}");
+                Console.WriteLine($"b = {b}");
+                return $"{a},{b}";
+            });
+            Console.WriteLine($"explain the method with print befor add and after (1): \n{output}");
+
+            Console.WriteLine("\n--------------");
+            
+            var output1 = names.Aggregate((a, b) => $"{a},{b}");
+            Console.WriteLine($"the same (1):\n {output}");
+
+        }
+
+        private static void RunMethod02()
+        {
+            var numbers = new[] { 1, 2, 3, 4, 5 };
+
+            //var total = 0;
+            //foreach (var n in numbers)
+            //    total += n;
+
+
+            //example:initial value = 0
+            var total = numbers.Aggregate(0, (a, b) => a + b);
+            Console.WriteLine($"Total with initial value = 0:\n{total}");
+
+            Console.WriteLine("\n--------------");
+
+            //example:initial value = 2
+            var total1 = numbers.Aggregate(2, (a, b) => a + b);
+            Console.WriteLine($"Total with initial value = 2:\n {total1}");
+        }
+
+        /// <summary>
+        /// get the longest title in the all questions
+        /// </summary>
+        private static void RunMethod03()
+        {
+            var quiz = QuestionBank.All;
+
+            //give initial value (assum first question has longest title)
+            var longestTitle = quiz[0];
+            Console.WriteLine($"{longestTitle}");
+            Console.WriteLine("\n------------\n");
+            var longestQuestionTitle = quiz.Aggregate(longestTitle,
+                (longest, next) => longest.Title.Length < next.Title.Length ? next : longest, x =>x);
+
+            Console.WriteLine(longestQuestionTitle);
+
+        }
+    }
+}
---
+++ LINQTut14.StandardMethod/Program.cs
@@ -0,0 +1,91 @@
+﻿
+using Shared;
+
+namespace LINQTut14.StandardMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RunCount();
+            Console.WriteLine("\n***************");
+
+            RunMax();
+            Console.WriteLine("\n***************");
+            
+            RunMaxBy();
+            Console.WriteLine("\n***************");
+
+            RunMin();
+            Console.WriteLine("\n***************");
+
+            RunMinBy();
+            Console.WriteLine("\n***************");
+
+            RunSum();
+            Console.WriteLine("\n***************");
+
+            RunAvg();
+            Console.ReadKey();
+        }
+
+        private static void RunCount()
+        {
+            var quiz = QuestionBank.GetQuestionRange(Enumerable.Range(1, 200));
+            Console.WriteLine($"Total Questions: {quiz.Count()}");
+            Console.WriteLine($"Total Questions With One Mark : {quiz.Count(x => x.Marks == 1)}");
+            Console.WriteLine($"Total Questions With One Mark : {quiz.Where(x => x.Marks == 1).Count()}");
+            Console.WriteLine($"Total Questions With One Mark Using LongCount : {quiz.Where(x => x.Marks == 1).LongCount()}");
+        }
+
+        /// <summary>
+        /// return the max marks from quistion
+        /// </summary>
+        private static void RunMax()
+        {
+            var quiz = QuestionBank.GetQuestionRange(Enumerable.Range(1, 200));
+            var maximumMark = quiz.Max(x => x.Marks);
+            //var maximumMark = quiz.Where(x => x.Choices.Count < 3).Max(x => x.Marks);
+            Console.WriteLine($"Maximum Mark: {maximumMark}");
+        }
+
+        /// <summary>
+        /// return the question which has the max marks 
+        /// </summary>
+        private static void RunMaxBy()
+        {
+            var quiz = QuestionBank.GetQuestionRange(Enumerable.Range(1, 200));
+            var maximumQuestionMark = quiz.MaxBy(x => x.Marks);
+            //var maximumMark = quiz.Where(x => x.Choices.Count < 3).Max(x => x.Marks);
+            Console.WriteLine($"{maximumQuestionMark}");
+        }
+        private static void RunMin()
+        {
+            var quiz = QuestionBank.GetQuestionRange(Enumerable.Range(1, 200));
+            var maximumMark = quiz.Min(x => x.Marks);
+            //var minimumMark = quiz.Where(x => x.Choices.Count < 3).Min(x => x.Marks);
+            Console.WriteLine($"Minimum Mark: {maximumMark}");
+        }
+        private static void RunMinBy()
+        {
+            var quiz = QuestionBank.GetQuestionRange(Enumerable.Range(1, 200));
+            var minimumQuestionMark = quiz.MinBy(x => x.Marks);
+            //var minimumMark = quiz.Where(x => x.Choices.Count < 3).MinBy(x => x.Marks);
+            Console.WriteLine($"{minimumQuestionMark}");
+        }
+
+        private static void RunSum()
+        {
+            var quiz = QuestionBank.GetQuestionRange(Enumerable.Range(1, 200));
+            var total = quiz.Sum(x => x.Marks);
+            Console.WriteLine($"total: {total}");
+        }
+
+        private static void RunAvg()
+        {
+            var quiz = QuestionBank.GetQuestionRange(Enumerable.Range(1, 200));
+            var average = quiz.Average(x => x.Marks);
+            Console.WriteLine($"average: {average.ToString("#.##")}");
+        }
+    }
+}
